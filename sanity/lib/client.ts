9285757991949290fe1c81c71d685d6a942d6150
maksymshtarkberg <starkberg@icloud.com>
2024-04-15 22:06:25 +0300

    deployment

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,12 +1,15 @@
-import { createClient } from "next-sanity"
+import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId, useCdn } from "../env"
+
+
+import { apiVersion, dataset, projectId, useCdn } from "../env";
+
 
 export const client = createClient({
   apiVersion,
   dataset,
   projectId,
   useCdn,
-  token:
-    "skdHNLi5PzO89J0FCiVb2aTouoJWG15BjhC3thQBrI5V0YBMFwVZtJyE5OIRx3JWfQVGYbGUii3eNbXandK8VW5QB4vuHcEJCPzuSRd90s6gvUbRjHS7KhWApkV2TuubdJ2rcJ0qjU8GxYxMdNMo1Wa3W45HHpBNiRfuLsfhNOFkgG4gQY7i",
+  // token:
+  //   "skdHNLi5PzO89J0FCiVb2aTouoJWG15BjhC3thQBrI5V0YBMFwVZtJyE5OIRx3JWfQVGYbGUii3eNbXandK8VW5QB4vuHcEJCPzuSRd90s6gvUbRjHS7KhWApkV2TuubdJ2rcJ0qjU8GxYxMdNMo1Wa3W45HHpBNiRfuLsfhNOFkgG4gQY7i",
 })
